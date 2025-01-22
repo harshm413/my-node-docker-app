dbded359d4afd51e0f4d1697e24ee401c4f09254
harshm413 <119815007+harshm413@users.noreply.github.com>
2025-01-22 09:56:05 +0530

    project updated

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js Server is Working!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
